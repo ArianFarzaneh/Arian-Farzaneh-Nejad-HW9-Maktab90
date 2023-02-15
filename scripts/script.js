a397ec67d3cc4f84e4b93f1a58a3164032d8bd19
Arian Farzanehnejad <afn2681@gmail.com>
2023-02-15 21:05:30 +0330

    add the answer of q8

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 'use strict'
+const closeOne=document.getElementById("closeOne")
+const closeTwo=document.getElementById("closeTwo")
+const closeThree=document.getElementById("closeThree")
+
+closeOne.onclick=function()
+{
+    closeOne.closest("div").remove()
+}
+
+closeTwo.onclick=function()
+{
+    closeTwo.closest("div").remove()
+}
+
+closeThree.onclick=function()
+{
+    closeThree.closest("div").remove()
+}
