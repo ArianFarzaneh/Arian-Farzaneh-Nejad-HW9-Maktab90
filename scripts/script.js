389924ee338bbdaa31a73f0fd4f8585dbc927ad6
Arian Farzanehnejad <afn2681@gmail.com>
2023-02-15 21:02:00 +0330

    add the answer of q7

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,24 @@
 'use strict'
+const title=document.getElementById("title")
+const li = document.querySelectorAll(".li")
+title.onclick=function()
+{
+    if(li[1].style.display==="block")
+    {
+        for(let listItem of li)
+        {
+            // if()
+            listItem.style.display="none"
+    
+        }
+    }
+    else
+    {
+        for(let listItem of li)
+        {
+            // if()
+            listItem.style.display="block"
+    
+        }
+    }
+}
