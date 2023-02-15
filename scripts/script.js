b321c63377574a01d74e0f062b0d01869bd370d8
Arian Farzanehnejad <afn2681@gmail.com>
2023-02-15 21:35:43 +0330

    add the answer of q9

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,50 @@
 'use strict'
+// const closeOne=document.getElementById("closeOne")
+// const closeTwo=document.getElementById("closeTwo")
+// const closeThree=document.getElementById("closeThree")
+const tooltip1=document.getElementById("tooltip1")
+const tooltip2=document.getElementById("tooltip2")
+const tooltip3=document.getElementById("tooltip3")
+
+// closeOne.onclick=function()
+// {
+//     closeOne.closest("div").remove()
+// }
+
+// closeTwo.onclick=function()
+// {
+//     closeTwo.closest("div").remove()
+// }
+
+// closeThree.onclick=function()
+// {
+//     closeThree.closest("div").remove()
+// }
+function toolTip1()
+{
+    tooltip1.style.display="block"
+}
+
+function toolTip2()
+{
+    tooltip2.style.display="block"
+}
+
+function toolTip3()
+{
+    tooltip3.style.display="block"
+}
+function toolTip1close()
+{
+    tooltip1.style.display="none"
+}
+
+function toolTip2close()
+{
+    tooltip2.style.display="none"
+}
+
+function toolTip3close()
+{
+    tooltip3.style.display="none"   
+}
