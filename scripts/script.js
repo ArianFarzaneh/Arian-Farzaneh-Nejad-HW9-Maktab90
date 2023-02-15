c276a871a4594a1f0e260dce30abbea5aceeef00
Arian Farzanehnejad <afn2681@gmail.com>
2023-02-15 20:50:37 +0330

    add the answer of q3

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,24 @@
 'use strict'
+
+
+let userInput
+let li
+function liCreator()
+{
+    const ul=document.createElement("ul")
+    document.body.appendChild(ul)
+    while(1)
+    {
+        userInput=prompt()
+        if(userInput==="" || userInput===null) //(for empty srtings and Esc btn)
+        break
+        else
+        {
+            li= document.createElement("li")
+            li.innerText=userInput
+            ul.appendChild(li)
+
+        }
+    }
+}
+liCreator()
