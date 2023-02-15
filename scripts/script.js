67e8188ff88260eedbeed052e62cc0f4c9b6e0da
Arian Farzanehnejad <afn2681@gmail.com>
2023-02-15 20:55:40 +0330

    add the answer of q5

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 'use strict'
+const elem=document.getElementById("elem")
+function clear() {
+    elem.innerHTML=""
+}
+setTimeout(clear,3000)
+function showElem()
+{
+    console.log(elem);
+}
+setTimeout(showElem,3001)//neshan dadan node bad az pak kardan mohtaviat an
