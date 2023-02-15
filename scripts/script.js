17831386fe92ad8ee8a89797a33a55ffc8c14ce1
Arian Farzanehnejad <afn2681@gmail.com>
2023-02-15 20:43:54 +0330

    add question1 answer

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,29 @@
 'use strict'
+
+//1:
+const table=document.getElementById("age-table")
+console.log(table);
+
+//2:
+let labels = document.querySelectorAll('td > label')
+for(let label of labels)
+{
+    console.log(label);
+}
+
+
+//3:
+const firstTd = document.querySelector("td:first-child")
+console.log(firstTd);
+
+//4:
+const searchForm=document.getElementsByName("search")[0]//return the first node with name search
+console.log(searchForm);
+
+//5:
+const fisrtInputInSearchForm=document.querySelector("input")//return the first input
+console.log(fisrtInputInSearchForm);
+
+//6:
+const lastInputInSearchForm=document.querySelector("form > input:last-child")
+console.log(lastInputInSearchForm);
