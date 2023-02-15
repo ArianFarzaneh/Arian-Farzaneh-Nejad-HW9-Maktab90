024197a3e64fd4a1f3b02b45f6914e6f3a68c02c
Arian Farzanehnejad <afn2681@gmail.com>
2023-02-15 20:58:49 +0330

    add the answer of q6

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
 'use strict'
+const button=document.getElementById("hider")
+const text=document.getElementById("text")
+function cleardiv()
+{
+    text.remove()
+}
+function clearButton()
+{
+    button.remove()
+}
+button.onclick=function()
+{
+    cleardiv()
+    setTimeout(clearButton,2000)
+}
